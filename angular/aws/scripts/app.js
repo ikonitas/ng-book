31494b05a70b0f9154c974c68d8bf4ac5550f299
Edvinas Jurevicius <ikonitas@gmail.com>
2014-08-24 21:00:01 +0100

    Add g+ signing button

--- angular/aws/scripts/app.js
+++ angular/aws/scripts/app.js
@@ -1,3 +1,10 @@
+window.onLoadCallback=function() {
+  angular.element(document).ready(function() {
+    gapi.client.load('oauth2','v2',function() {
+      angular.bootstrap(document, ['myApp']);
+    });
+  });
+}
 angular.module('myApp', [
     'ngRoute',
     'myApp.services',
